423e604b0eb937b0610bc4f812309066f9ab0ced
Tanattha <tanattha.thuenim@gmail.com>
2020-07-08 19:53:52 -0400

    updated nodejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require('@fortawesome/fontawesome-free/js/all');
+
 require('jquery');
 
 require('bootstrap');
