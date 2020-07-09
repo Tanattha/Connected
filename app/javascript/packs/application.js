cfe8f4f0fef0ff907e1042aca69401f9a57fea4d
Tanattha <tanattha.thuenim@gmail.com>
2020-07-08 20:57:04 -0400

    updated js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
 require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-
+//require("turbolinks").start();
+//require("@rails/activestorage").start();
+//require("channels");
 require('jquery');
+//require('bootstrap');
+//require("trix");
 
-require('bootstrap');
-require("trix");
-require("@rails/actiontext");
-
-
-require("stylesheets/application");
-import "controllers";
+// ...
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+//= jquery_ujs
+//= require jquery_ujs
 
