4bb316f23e3c46c5fdba36d421ab813abd235ac0
Tanattha <tanattha.thuenim@gmail.com>
2020-07-08 22:10:21 -0400

    updated

--- config/boot.rb
+++ config/boot.rb
@@ -2,12 +2,3 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-require 'rails/commands/server'
-
-module Rails
-  class Server
-    def default_options
-      super.merge({Port: 10524})
-    end
-  end
-end
