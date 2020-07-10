b3ab6b720c9e076c01b6fce12665df3b1d2340a9
Tanattha <tanattha.thuenim@gmail.com>
2020-07-10 14:22:50 -0400

    updated name

--- config/aws.rb
+++
@@ -1,7 +0,0 @@
-Aws.config.update({
-  region: 'us-east-2',
-  credentials: Aws::Credentials.new(ENV['BUCKETEER_AWS_ACCESS_KEY_ID'], ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKETEER_BUCKET_NAME'])
-
