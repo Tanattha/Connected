d03ff2843ac75636ede7749bcc0011b24856235a
Tanattha <tanattha.thuenim@gmail.com>
2020-07-10 21:22:57 -0400

    change controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    before_action :logged_in?, only: [:new, :create, :update, :edit]
-  
+    before_action :logged_in?
+    skip_before_action :logged_in?, only: [:show]
     def new
         @user = User.new
     end
