c4b805af7b6c782bed9e8c2ddb5a9c99258d3daf
Tanattha <tanattha.thuenim@gmail.com>
2020-07-10 21:18:35 -0400

    changed user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-    before_action :logged_in?
-    skip_before_action :logged_in?, only: [:new, :create]
+    before_action :logged_in?, only: [:new, :create, :update, :edit]
   
     def new
         @user = User.new
