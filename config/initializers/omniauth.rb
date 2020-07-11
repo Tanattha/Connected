2754b8035902cc7e3a70a64010bbd3fea0a7758c
Tanattha <tanattha.thuenim@gmail.com>
2020-07-10 20:53:45 -0400

    added omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], skip_jwt: true
 end
 
+OmniAuth.config.full_host = Rails.env.production? ? 'https://rails-connected.herokuapp.com' : 'http://localhost:3000'
